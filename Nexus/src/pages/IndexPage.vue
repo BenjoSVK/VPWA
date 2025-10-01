b008fb4b458e1ff1ac075602caefded13d157ae2
Martin Stavrovsky <121042775+MartinStav@users.noreply.github.com>
2025-10-01 12:02:16 +0200

    Add global styles and navigation to IndexPage

--- Nexus/src/pages/IndexPage.vue
+++ Nexus/src/pages/IndexPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <q-page />
+  <q-page class="glass-demo">
+    <div class="glass-demo__background">Glass</div>
+
+    <div class="glass-demo__card glass-bg ">
+      <h1>Glass background demo</h1>
+      <p>
+        Uprav triedu <code>.glass-bg</code> v <code>app.scss</code> a sleduj ako sa mení vzhľad
+        tohto bloku.
+      </p>
+    </div>
+  </q-page>
 </template>
 
 <script setup lang="ts">
